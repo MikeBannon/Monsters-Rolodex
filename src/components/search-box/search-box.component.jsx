ed9006646eec5972e46a5a2d8ad66d279f2e2594
Mike Bannon <mike.bannon@newworlddigitalmedia.co.uk>
2023-01-25 19:27:30 +0000

    adding files

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import "./search-box.styles.css";
+
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+	<input
+		className={`search-box ${className}`}
+		placeholder={placeholder}
+		onChange={onChangeHandler}
+	/>
+);
+
+export default SearchBox;
